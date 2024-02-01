2406dcec83ef643d8fc08d4a1bb1ddcaef6d1804
taako-502 <i.am.armorknight@gmail.com>
2024-02-01 12:05:33 +0200

    refactor

--- main.go
+++ main.go
@@ -7,84 +7,73 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/pkg/errors"
 )
 
 const ITERATIONS = 10000
 
-func main() {
-	fmt.Println("# 対称鍵署名アルゴリズム")
-	fmt.Println("## HS256")
-	secretKey, err := generate_key.GenerateHS256Key()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	s := sign_benchmark.NewSignReciever(secretKey)
-	duration, err := s.SignatureAlgorithm(jwt.SigningMethodHS256, ITERATIONS)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	printDuration(duration)
-
-	fmt.Println("◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎")
-	fmt.Println("# 非対称鍵署名アルゴリズム")
-	fmt.Println("## Ed25519")
-	_, privateKey, err := generate_key.GenerateEd25519KeyPair()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	a := sign_benchmark.NewSignReciever(privateKey)
-	duration, err = a.SignatureAlgorithm(&jwt.SigningMethodEd25519{}, ITERATIONS)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	printDuration(duration)
-
-	fmt.Println("◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎")
-	fmt.Println("## RS256")
-	_, rsaPrivateKey, err := generate_key.GenerateRSAKeyPair(2048) // 通常は2048または4096
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	a = sign_benchmark.NewSignReciever(rsaPrivateKey)
-	duration, err = a.SignatureAlgorithm(jwt.SigningMethodRS256, ITERATIONS)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	printDuration(duration)
-
-	fmt.Println("◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎")
-	fmt.Println("## ES256")
-	_, ecdsaPrivateKey, err := generate_key.GenerateECDSAKeyPair()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	a = sign_benchmark.NewSignReciever(ecdsaPrivateKey)
-	duration, err = a.SignatureAlgorithm(jwt.SigningMethodES256, ITERATIONS)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	printDuration(duration)
-
-	fmt.Println("◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎◻︎◾︎")
-	fmt.Println("## PS256")
-	a = sign_benchmark.NewSignReciever(rsaPrivateKey) // RS256と同じ鍵を使う
-	duration, err = a.SignatureAlgorithm(jwt.SigningMethodPS256, ITERATIONS)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	printDuration(duration)
+type algorithm struct {
+	secretKey interface{}
+	label     string
+	method    jwt.SigningMethod
 }
 
-func printDuration(duration time.Duration) {
-	milliseconds := float64(duration) / float64(time.Millisecond)
-	fmt.Printf("JWT署名生成（10,000回）にかかった時間: %.3f ms\n", milliseconds)
+func main() {
+	algorithms, err := buildAlgorithms()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, method := range algorithms {
+		s := sign_benchmark.NewSignReciever(method.secretKey)
+		duration, err := s.SignatureAlgorithm(method.method, ITERATIONS)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("")
+		fmt.Printf("## %s\r\n", method.label)
+		milliseconds := float64(duration) / float64(time.Millisecond)
+		fmt.Printf("JWT署名生成（10,000回）にかかった時間: %.3f ms\n", milliseconds)
+	}
+
+	fmt.Println("")
+	fmt.Println("終了")
+}
+
+func buildAlgorithms() ([]algorithm, error) {
+	var algorithms []algorithm
+	// HS256
+	secretKey, err := generate_key.GenerateHS256Key()
+	if err != nil {
+		return nil, errors.Wrap(err, "generate_key.GenerateHS256Key")
+	}
+	algorithms = append(algorithms, algorithm{secretKey: secretKey, label: "HS256", method: jwt.SigningMethodHS256})
+
+	// Ed25519
+	_, privateKey, err := generate_key.GenerateEd25519KeyPair()
+	if err != nil {
+		return nil, errors.Wrap(err, "generate_key.GenerateEd25519KeyPair")
+	}
+	algorithms = append(algorithms, algorithm{secretKey: privateKey, label: "Ed25519", method: &jwt.SigningMethodEd25519{}})
+
+	// RS256
+	_, rsaPrivateKey, err := generate_key.GenerateRSAKeyPair(2048) // 通常は2048または4096
+	if err != nil {
+		return nil, errors.Wrap(err, "generate_key.GenerateRSAKeyPair")
+	}
+	algorithms = append(algorithms, algorithm{secretKey: rsaPrivateKey, label: "RS256", method: jwt.SigningMethodRS256})
+
+	// ES256
+	_, ecdsaPrivateKey, err := generate_key.GenerateECDSAKeyPair()
+	if err != nil {
+		return nil, errors.Wrap(err, "generate_key.GenerateECDSAKeyPair")
+	}
+	algorithms = append(algorithms, algorithm{secretKey: ecdsaPrivateKey, label: "ES256", method: jwt.SigningMethodES256})
+
+	// PS256
+	algorithms = append(algorithms, algorithm{secretKey: rsaPrivateKey, label: "PS256", method: jwt.SigningMethodPS256})
+
+	return algorithms, nil
 }
